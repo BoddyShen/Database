01873524063dc8e0ee0fd505cd08eddbf8182fdc
TomNgo03 <ngoquangtung281203@gmail.com>
2025-04-04 20:01:30 -0400

    edge cases for lru_cache

--- lab2/test/unit/test_lru_cache.cpp
+++ lab2/test/unit/test_lru_cache.cpp
@@ -61,6 +61,53 @@ void test_remove() {
     std::cout << "remove() passed!" << std::endl;
 }
 
+// Test updating existing elements
+void test_update_existing() {
+    std::cout << "Testing update of existing elements..." << std::endl;
+    LRUCache cache(3);
+    
+    // Add initial elements
+    cache.put(1);
+    cache.put(2);
+    cache.put(3);
+    
+    // Update element multiple times
+    cache.put(2);
+    cache.put(2);
+    cache.put(2);
+    
+    assert(cache.getSize() == 3);  // Size shouldn't change
+    
+    Node* firstNode = cache.getFirstNode();
+    assert(firstNode->val == 1);
+    assert(firstNode->next->val == 3);
+    assert(firstNode->next->next->val == 2);  // 2 should be most recently used
+    
+    std::cout << "Update existing elements test passed!" << std::endl;
+}
+
+// Test empty cache operations
+void test_empty_cache() {
+    std::cout << "Testing empty cache operations..." << std::endl;
+    LRUCache cache(3);
+    
+    assert(cache.getSize() == 0);  // Initial size should be 0
+    
+    // Add and remove to get back to empty
+    cache.put(1);
+    assert(cache.getSize() == 1);
+    cache.remove(1);
+    assert(cache.getSize() == 0);
+    
+    // Add multiple and remove all
+    cache.put(1);
+    cache.put(2);
+    cache.remove(1);
+    cache.remove(2);
+    assert(cache.getSize() == 0);
+    
+    std::cout << "Empty cache operations test passed!" << std::endl;
+}
 
 int main() {
     std::cout << "### Running LRUCache Tests ###" << std::endl;
@@ -68,7 +115,9 @@ int main() {
     test_put();
     testLRUBehavior();
     test_remove();
+    test_update_existing();
+    test_empty_cache();
     
-    std::cout << "tests passed" << std::endl;
+    std::cout << "All tests passed!" << std::endl;
     return 0;
 }
