93beb2ac6c40708bbf3abac3819c17c159ef477a
Po-Heng Shen <f2213851026@gmail.com>
2025-05-04 20:11:53 -0400

    fix: fix estimated I/O

--- lab3/include/Commands.h
+++ lab3/include/Commands.h
@@ -9,6 +9,7 @@ void pre_process(bool test = false);
 // Execute the director-lookup query with title in [start,end],
 // using buffer_size pages in your BufferManager.
 void run_query(const std::string &start_range, const std::string &end_range, int buffer_size,
-               bool test = false, double *selectivity = nullptr, int *ioCount = nullptr);
+               bool test = false, double *selectivity = nullptr, int *join1tuples = nullptr,
+               int *ioCount = nullptr);
 
 #endif // COMMANDS_H
